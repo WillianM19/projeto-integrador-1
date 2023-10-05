8238323ddd0a35867b0bca76869a0e4fa8e7cf5d
kennedywc <kennedy.costan10@gmail.com>
2023-10-05 14:08:26 -0300

    atualização de pastas para atomic design

--- jornal_web/views.py
+++ jornal_web/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(
         request,
-        'jornal_web/index.html',          
+        'pages/index.html',          
     )
