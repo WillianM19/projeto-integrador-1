b6a3339f13cfc1f76c753f169162504cd3d66709
kennedywc <kennedy.costan10@gmail.com>
2023-10-18 09:21:54 -0300

    Alguns ajustes e criação do html base)

--- jornal_web/urls.py
+++ jornal_web/urls.py
@@ -4,4 +4,6 @@ from jornal_web import views
 # blog/
 urlpatterns = [
     path('', views.home, name='home'),
+    # para testar os componentes
+    path('components', views.components, name='components'),
 ]
