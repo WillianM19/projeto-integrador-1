942b4431578dbd5661c0d33efe7c27c0f18cbf0f
kennedywc <kennedy.costan10@gmail.com>
2023-10-21 20:27:24 -0300

    Carde lista de eventos

--- jornal_web/views.py
+++ jornal_web/views.py
@@ -7,7 +7,7 @@ def components(request):
     
 
     # highlights
-    admin = True
+    admin = False
 
     destaques_data = [
     {
@@ -30,18 +30,76 @@ def components(request):
         'imagem': 'media/img/highlights/5.png',
         'titulo': 'Aprendizado Global: Alunos da Escola VWX Participam de Intercâmbio Cultural',
     },]
-    destaques_data = {
-        'week': 'set',
-        'dayWeek': '12',
-        'title': 'Feira de Ciências',
-        'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
-        'event_datetime': '2023-10-17T14:00',
-    }
+
+    event_list = [
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+                {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+        {
+            'week': 'set',
+            'dayWeek': '12',
+            'title': 'Feira de Ciências',
+            'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+            'event_datetime': '2023-10-17T14:00',
+        },
+    ]
 
 
     return render(
         request,
-        'pages/components.html', {'tags': staticTags, 'admin': admin, 'destaques_data': destaques_data}
+        'pages/components.html', {'tags': staticTags, 'admin': admin, 'event_list': event_list}
     )
 
 
