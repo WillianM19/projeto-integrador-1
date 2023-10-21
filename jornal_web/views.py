c496921353a7b3354adf0f8fa009cb38cc48a6c2
kennedywc <kennedy.costan10@gmail.com>
2023-10-21 14:56:46 -0300

    Componente destaque para eventos

--- jornal_web/views.py
+++ jornal_web/views.py
@@ -30,7 +30,15 @@ def components(request):
         'imagem': 'media/img/highlights/5.png',
         'titulo': 'Aprendizado Global: Alunos da Escola VWX Participam de Intercâmbio Cultural',
     },]
-    
+    destaques_data = {
+        'week': 'set',
+        'dayWeek': '12',
+        'title': 'Feira de Ciências',
+        'event_date': '17 de outubro de 2023 Das 14:00 às 17:00',
+        'event_datetime': '2023-10-17T14:00',
+    }
+
+
     return render(
         request,
         'pages/components.html', {'tags': staticTags, 'admin': admin, 'destaques_data': destaques_data}
