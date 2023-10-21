efb883084d0352f2d3ccd95445e03cc1c9485b66
WillianM19 <willianm1928@gmail.com>
2023-10-21 16:18:33 -0300

    Finalizando página delogin

--- jornal_web/urls.py
+++ jornal_web/urls.py
@@ -4,6 +4,7 @@ from jornal_web import views
 # blog/
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login', views.login, name='login'),
     # para testar os componentes
     path('components', views.components, name='components'),
 ]
--- jornal_web/views.py
+++ jornal_web/views.py
@@ -16,3 +16,10 @@ def home(request):
         request,
         'pages/home.html', {'tags': staticTags}
     )
+    
+def login(request):
+    
+    return render(
+        request,
+        'pages/login.html'
+    )
