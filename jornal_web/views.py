298e4cac7a8f7f31d7cb3ada1295e9a8cbbfc453
WillianM19 <willianm1928@gmail.com>
2023-10-22 19:41:33 -0300

    Corrindo componente de destaques e eventos, e adicionando overflow no texto

--- jornal_web/views.py
+++ jornal_web/views.py
@@ -9,7 +9,17 @@ def components(request):
     # highlights
     admin = False
 
-    destaques_data = [
+
+    return render(
+        request,
+        'pages/components.html', {'tags': staticTags, 'admin': admin, 'event_list': event_list}
+    )
+
+
+def home(request):
+    staticTags = ['Artigos', 'Eventos', 'Notícias', 'Tecnologia', 'Ciência e Pesquisa', 'Dicas de Estudo', 'Boas Praticas Escolares', 'Recursos Educationais', 'Notícias', 'Tecnologia', 'Ciência e Pesquisa', 'Dicas de Estudo', 'Boas Praticas Escolares', 'Recursos Educationais']
+    
+    relevantSliderContent = [
     {
         'imagem': 'media/img/highlights/1.png',
         'titulo': 'Aprendizado Global: Alunos da Escola VWX Participam de Intercâmbio Cultural',
@@ -30,7 +40,7 @@ def components(request):
         'imagem': 'media/img/highlights/5.png',
         'titulo': 'Aprendizado Global: Alunos da Escola VWX Participam de Intercâmbio Cultural',
     },]
-
+    
     event_list = [
         {
             'week': 'set',
@@ -95,20 +105,10 @@ def components(request):
             'event_datetime': '2023-10-17T14:00',
         },
     ]
-
-
-    return render(
-        request,
-        'pages/components.html', {'tags': staticTags, 'admin': admin, 'event_list': event_list}
-    )
-
-
-def home(request):
-    staticTags = ['Artigos', 'Eventos', 'Notícias', 'Tecnologia', 'Ciência e Pesquisa', 'Dicas de Estudo', 'Boas Praticas Escolares', 'Recursos Educationais', 'Notícias', 'Tecnologia', 'Ciência e Pesquisa', 'Dicas de Estudo', 'Boas Praticas Escolares', 'Recursos Educationais']
     
     return render(
         request,
-        'pages/home.html', {'tags': staticTags}
+        'pages/home.html', {'tags': staticTags, 'relevantSliderContent': relevantSliderContent, 'event_list': event_list}
     )
     
 def login(request):
