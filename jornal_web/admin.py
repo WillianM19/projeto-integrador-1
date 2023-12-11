7630ad0a8256df7f0049e9c91688df46cbfbec23
WillianM19 <willianm1928@gmail.com>
2023-12-11 15:00:36 -0300

    Models do projeto

--- jornal_web/admin.py
+++ jornal_web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Postador, Publicacao, Tags
 
 # Register your models here.
+admin.site.register(Postador)
+admin.site.register(Publicacao)
+admin.site.register(Tags)
--- projeto_integrador_1/settings.py
+++ projeto_integrador_1/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Imagens
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_images')
 
 # Application definition
 
